7697c81de0765351da0d80c99271880d96e9cd45
soymd <pacificstate33@gmail.com>
2022-11-11 16:30:15 +0900

    finish

--- src/main/kotlin/Poker.kt
+++ src/main/kotlin/Poker.kt
@@ -1,14 +1,77 @@
-class Poker {
-    fun judge(black: List<Card>, white: List<Card>):String {
-        val blackHand = Cards(black).getHand()
-        val whiteHand = Cards(white).getHand()
+import Suit.*
+import Value.*
 
-        if(blackHand == whiteHand) {
-            return "Tie"
-        } else if (blackHand.ordinal > whiteHand.ordinal){
-            return "Black wins - ${blackHand}"
+class Poker {
+    fun judge(black: String, white: String): String {
+        val blackHands = black.split(" ").map { map(it) }
+        val whiteHands = white.split(" ").map { map(it) }
+        val blackHand = Cards(blackHands).getHand()
+        val whiteHand = Cards(whiteHands).getHand()
+
+        return if (blackHand == whiteHand) {
+            "Tie"
+        } else if (blackHand.ordinal > whiteHand.ordinal) {
+            "Black wins - ${blackHand}"
         } else {
-            return "White wins - ${whiteHand}"
+            "White wins - ${whiteHand}"
         }
+    }
+
+    private fun map(str: String): Card {
+        val cardMap = mapOf(
+            "1D" to Card(DIAMOND, ACE),
+            "2D" to Card(DIAMOND, TWO),
+            "3D" to Card(DIAMOND, THREE),
+            "4D" to Card(DIAMOND, FOUR),
+            "5D" to Card(DIAMOND, FIVE),
+            "6D" to Card(DIAMOND, SIX),
+            "7D" to Card(DIAMOND, SEVEN),
+            "8D" to Card(DIAMOND, EIGHT),
+            "9D" to Card(DIAMOND, NINE),
+            "TD" to Card(DIAMOND, TEN),
+            "JD" to Card(DIAMOND, JACK),
+            "QD" to Card(DIAMOND, QUEEN),
+            "KD" to Card(DIAMOND, KING),
+            "1C" to Card(CLUB, ACE),
+            "2C" to Card(CLUB, TWO),
+            "3C" to Card(CLUB, THREE),
+            "4C" to Card(CLUB, FOUR),
+            "5C" to Card(CLUB, FIVE),
+            "6C" to Card(CLUB, SIX),
+            "7C" to Card(CLUB, SEVEN),
+            "8C" to Card(CLUB, EIGHT),
+            "9C" to Card(CLUB, NINE),
+            "TC" to Card(CLUB, TEN),
+            "JC" to Card(CLUB, JACK),
+            "QC" to Card(CLUB, QUEEN),
+            "KC" to Card(CLUB, KING),
+            "1S" to Card(SPADE, ACE),
+            "2S" to Card(SPADE, TWO),
+            "3S" to Card(SPADE, THREE),
+            "4S" to Card(SPADE, FOUR),
+            "5S" to Card(SPADE, FIVE),
+            "6S" to Card(SPADE, SIX),
+            "7S" to Card(SPADE, SEVEN),
+            "8S" to Card(SPADE, EIGHT),
+            "9S" to Card(SPADE, NINE),
+            "TS" to Card(SPADE, TEN),
+            "JS" to Card(SPADE, JACK),
+            "QS" to Card(SPADE, QUEEN),
+            "KS" to Card(SPADE, KING),
+            "1H" to Card(HEART, ACE),
+            "2H" to Card(HEART, TWO),
+            "3H" to Card(HEART, THREE),
+            "4H" to Card(HEART, FOUR),
+            "5H" to Card(HEART, FIVE),
+            "6H" to Card(HEART, SIX),
+            "7H" to Card(HEART, SEVEN),
+            "8H" to Card(HEART, EIGHT),
+            "9H" to Card(HEART, NINE),
+            "TH" to Card(HEART, TEN),
+            "JH" to Card(HEART, JACK),
+            "QH" to Card(HEART, QUEEN),
+            "KH" to Card(HEART, KING),
+        )
+        return cardMap[str]!!
     }
 }
--- src/test/kotlin/PokerTest.kt
+++ src/test/kotlin/PokerTest.kt
@@ -17,20 +17,22 @@ class PokerTest {
     @Test
     fun `judge return black win when black is straight flush and white is high card two`() {
         val actual = subject.judge(
-            listOf(
-                Card(HEART, TWO),
-                Card(HEART, THREE),
-                Card(HEART, FOUR),
-                Card(HEART, FIVE),
-                Card(HEART, SIX),
-            ),
-            listOf(
-                Card(HEART, TWO),
-                Card(DIAMOND, ACE),
-                Card(SPADE, FOUR),
-                Card(CLUB, FIVE),
-                Card(HEART, SIX),
-            )
+            "2H 3H 4H 5H 6H",
+//            listOf(
+//                Card(HEART, TWO),
+//                Card(HEART, THREE),
+//                Card(HEART, FOUR),
+//                Card(HEART, FIVE),
+//                Card(HEART, SIX),
+//            ),
+            "2H 3D 5S 9C KD"
+//            listOf(
+//                Card(HEART, TWO),
+//                Card(DIAMOND, ACE),
+//                Card(SPADE, FOUR),
+//                Card(CLUB, FIVE),
+//                Card(HEART, SIX),
+//            )
         )
 
         assertThat(actual, equalTo("Black wins - STRAIGHT_FLUSH"))
@@ -39,20 +41,22 @@ class PokerTest {
     @Test
     fun `judge return white win when black is 3card and white is four card`() {
         val actual = subject.judge(
-            listOf(
-                Card(HEART, THREE),
-                Card(DIAMOND, THREE),
-                Card(SPADE, THREE),
-                Card(CLUB, FOUR),
-                Card(SPADE, TEN),
-            ),
-            listOf(
-                Card(HEART, TWO),
-                Card(CLUB, TWO),
-                Card(SPADE, TWO),
-                Card(DIAMOND, TWO),
-                Card(HEART, JACK),
-            )
+            "3H 3D 3S 4C TC",
+            "2H 2D 2S 2C TC",
+//            listOf(
+//                Card(HEART, THREE),
+//                Card(DIAMOND, THREE),
+//                Card(SPADE, THREE),
+//                Card(CLUB, FOUR),
+//                Card(SPADE, TEN),
+//            ),
+//            listOf(
+//                Card(HEART, TWO),
+//                Card(CLUB, TWO),
+//                Card(SPADE, TWO),
+//                Card(DIAMOND, TWO),
+//                Card(HEART, JACK),
+//            )
         )
 
         assertThat(actual, equalTo("White wins - FOUR_OF_A_KIND"))
@@ -61,20 +65,23 @@ class PokerTest {
     @Test
     fun `judge return Tie when black is straight flush and white is straight flush`() {
         val actual = subject.judge(
-            listOf(
-                Card(HEART, TWO),
-                Card(HEART, THREE),
-                Card(HEART, FOUR),
-                Card(HEART, FIVE),
-                Card(HEART, SIX),
-            ),
-            listOf(
-                Card(CLUB, TWO),
-                Card(CLUB, THREE),
-                Card(CLUB, FOUR),
-                Card(CLUB, FIVE),
-                Card(CLUB, SIX),
-            ),
+            "2H 2D 2S 2C TC",
+            "2H 2D 2S 2C TC",
+//
+//            listOf(
+//                Card(HEART, TWO),
+//                Card(HEART, THREE),
+//                Card(HEART, FOUR),
+//                Card(HEART, FIVE),
+//                Card(HEART, SIX),
+//            ),
+//            listOf(
+//                Card(CLUB, TWO),
+//                Card(CLUB, THREE),
+//                Card(CLUB, FOUR),
+//                Card(CLUB, FIVE),
+//                Card(CLUB, SIX),
+//            ),
         )
 
         assertThat(actual, equalTo("Tie"))
